23fe1b36d5931fc7f1a62da7a6e652752d2a7566
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-06-06 16:41:14 +0100

    read through a file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string workingfolder = "/home/adrien/";
+    StreamReader sr = new StreamReader(workingfolder + "input.txt");
+
+    string line = string.Empty;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+      string s = line;
+    }
+
+    Console.WriteLine("Hello World!");
+  }
+}
+
